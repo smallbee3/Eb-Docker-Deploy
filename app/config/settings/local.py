87ac14fa223dbb4bf9cfe38ce79f6944093643b3
smallbee <smallbee3@gmail.com>
2018-03-09 13:20:04 +0900

    EB-Docker-Deploy Project Start

---
+++ app/config/settings/local.py
@@ -0,0 +1,25 @@
+from .base import *
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+ALLOWED_HOSTS = []
+
+
+WSGI_APPLICATION = 'config.wsgi.local.application'
+
+# secrets = json.loads(open(SECRETS_LOCAL, 'rt').read())
+# DATABASES = secrets['DATABASES']
+
+
+#
+#
+# # Database
+# # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
