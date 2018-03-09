a48c4eaa34e25e6c9750acba7688e2ae15b15dbd
smallbee <smallbee3@gmail.com>
2018-03-09 14:37:20 +0900

    When DJANGO_SETTINGS_MODULE is not set, it is set by module:settings/__init__.py

--- app/config/settings/local.py
+++ app/config/settings/local.py
@@ -3,7 +3,11 @@ from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    '.elasticbeanstalk.com',
+]
 
 
 WSGI_APPLICATION = 'config.wsgi.local.application'
@@ -12,14 +16,14 @@ WSGI_APPLICATION = 'config.wsgi.local.application'
 # DATABASES = secrets['DATABASES']
 
 
-#
-#
-# # Database
-# # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-#
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+
+
+# Database
+# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
